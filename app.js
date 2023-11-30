d2d5973f7a42639b3b3acba875df123c74b950cf
Eugene <eugenetiron@yahoo.com>
2023-11-30 18:16:06 +0000

    Second Commit

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes') 
+
 
 // express app
 const app = express();
 
+//connect to MongoDB
+const dbURI = 'mongodb+srv://eugenetiron:VeryUnusual1@cluster.yti9k0b.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
+.then((result) => app.listen(8800))
+.catch((err)=>console.log(err))
+
+
 // listen for requests
-app.listen(3000);
+
 
 // register view engine
 app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended:true }));
+app.use(morgan('dev'));
+
 
 app.use((req, res, next) => {
   console.log('new request made:');
@@ -26,30 +39,26 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(morgan('dev'));
+
 
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
+// mongoose and mongo db sandbox routes
+
+
+// routes
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
-
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
-
+//blog routes
+app.use('/blogs',blogRoutes);
 // 404 page
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
